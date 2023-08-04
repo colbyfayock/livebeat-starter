80406767e0caa5f4b285527bce4646c87b4d8672
safiya siddiqui <93072195+safiya-siddiqui@users.noreply.github.com>
2023-08-04 23:05:16 +0530

    Create btn.js
    added new file

---
+++ btn.js
@@ -0,0 +1,6 @@
+<p className="flex justify-center gap-4">
+          <button className="font-medium hover:text-[#535bf2] cursor-pointer">Log Out</button>  
+          {/* <button className="font-medium hover:text-[#535bf2] cursor-pointer">Log Out</button>   */}
+          <Link href="/login">
+            <a className="font-medium text-inherit">Log In</a>
+          </Link>
